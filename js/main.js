86bb9d7619b5b6d5da27676c7075b444e84e0be3
arakitieva <evarnaut@gmail.com>
2020-09-06 23:15:17 +0500

    create: added footer, newsletter, reviews, stories

--- js/main.js
+++ js/main.js
@@ -1,39 +1,69 @@
-var mySwiper = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
+$(document).ready(function () {
 
-  // If we need pagination
-  pagination: {
-    el: '.reviews-pagination',
-    clickable: true,
-  },
+  var tabsItems = $(".tabs__item");
+  var contentItem = $(".tabs-content__item");
 
-  autoplay: {
-    delay: 8000,
-  },
+  tabsItems.on("click", function (event) {
+    var activeCotent = $(this).attr("data-target");
+    tabsItems.removeClass("tabs__item--active");
+    contentItem.removeClass("tabs-content__item--active");
+    $(activeCotent).addClass("tabs-content__item--active");
+    $(this).addClass("tabs__item--active");
+  });
 
-  on: {
-    init() {
-      this.el.addEventListener("mouseenter", () => {
-        this.autoplay.stop();
-      });
+  var mySwiper = new Swiper(".reviews-slider", {
+    // Optional parameters
+    loop: true,
 
-      this.el.addEventListener("mouseleave", () => {
-        this.autoplay.start();
-      });
+    // If we need pagination
+    pagination: {
+      el: ".reviews-pagination",
+      clickable: true,
     },
-  },
-});
 
-var storiesSlider = new Swiper(".stories-slider", {
-  // Navigation arrows
-  navigation: {
-    nextEl: ".stories-info__button--next",
-    prevEl: ".stories-info__button--prev",
-  },
+    autoplay: {
+      delay: 7000,
+    },
+    // Остановка слайдера при наведении курсора
+    on: {
+      init() {
+        this.el.addEventListener("mouseenter", () => {
+          this.autoplay.stop();
+        });
 
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
+        this.el.addEventListener("mouseleave", () => {
+          this.autoplay.start();
+        });
+      },
+    },
+  });
+
+  var storiesSlider = new Swiper(".stories-slider", {
+    // Optional parameters
+    // loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".stories-info__button--next",
+      prevEl: ".stories-info__button--prev",
+    },
+
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+
+  // обработка формы subscribe
+  $(".subscribe").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        email: {
+          required: "Field mail is required",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+      },
+    });
+  });
 });
